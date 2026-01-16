2805f9dc128865add4a4cccf2738900a258075a5
icanq <icanatawijaya@gmail.com>
2026-01-16 20:27:06 +0700

    feat: update API structure with new Links module, implement CRUD operations, and enhance testing setup with Jest

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,35 +1,22 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import { config as baseConfig } from '@workspace/eslint-config/base';
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+/** @type {import("eslint").Linter.Config} */
+export default [
+  ...baseConfig,
   {
     languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
       parserOptions: {
-        projectService: true,
+        project: './tsconfig.json',
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
     },
   },
-);
+  {
+    ignores: ['.prettierrc.mjs', 'eslint.config.mjs', 'dist/**'],
+  },
+];
---
+++ apps/api/src/links/dto/index.ts
@@ -0,0 +1,3 @@
+export { Link } from './link.entity';
+export { CreateLinkDto } from './create-link.dto';
+export { UpdateLinkDto } from './update-link.dto';
